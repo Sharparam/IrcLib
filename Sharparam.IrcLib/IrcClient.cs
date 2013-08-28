5879acdb42c3dbee922f18e2d031b6c588eab461
Sharparam <adam.hellberg95@gmail.com>
2013-08-28 14:32:19 +0200

    Start some coding, added license too

---
+++ Sharparam.IrcLib/IrcClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharparam.IrcLib
+{
+    public class IrcClient
+    {
+        public string Server { get; private set; }
+        public short Port { get; private set; }
+
+        public IrcClient(string server, short port)
+        {
+            
+        }
+    }
+}
